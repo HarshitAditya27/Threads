4a625f7f2e316aa7b6a1a1533afe397fa52e1397
Harshit Aditya <harshitaditya27@gmail.com>
2023-10-23 00:41:01 +0530

    Added comment functionality

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ThreadCard from "@/components/cards/ThreadCard";
+import Comment from "@/components/forms/Comment";
 import { fetchThreadById } from "@/lib/actions/thread.action";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
@@ -27,11 +28,30 @@ const page = async ({ params }: { params: { id: string } }) => {
           author={thread.author}
           community={thread.community}
           createdAt={thread.createdAt}
-          comments={thread.children}
+          comment={thread.children}
         />
       </div>
       <div className="mt-7">
-        <Comment />
+        <Comment
+          threadId={thread.id}
+          currentUserImg={userInfo.image}
+          currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
+      <div className="mt-10">
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={user?.id || ""}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comment={childItem.children}
+          />
+        ))}
       </div>
     </section>
   );
--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,8 +1,86 @@
-const Comment = () => {
+"use client";
+
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Button } from "../ui/button";
+import Image from "next/image";
+import { Input } from "../ui/input";
+
+// import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
+import { CommentValidation } from "@/lib/validations/thread";
+import { addCommentToThread, createThread } from "@/lib/actions/thread.action";
+import { currentUser } from "@clerk/nextjs";
+
+interface Props {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
+}
+
+const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
+  const form = useForm({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    await addCommentToThread(
+      threadId,
+      values.ThreadValidation,
+      JSON.parse(currentUserId),
+      pathname
+    );
+    router.push("/");
+  };
+
   return (
-    <div>
-      <h1>Comment Form</h1>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="comment-form">
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex w-full  gap-3 items-center">
+              <FormLabel className="text-base-semifold text-light-2">
+                <Image
+                  src={currentUserImg}
+                  alt="Profile image"
+                  width={48}
+                  height={48}
+                  className="rounded-full object-cover"
+                />
+              </FormLabel>
+              <FormControl className="border-none bg-transparent">
+                <Input
+                  className="no-focus text-light-1 outline-none"
+                  type="text"
+                  placeholder="Comment..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="comment-form_btn">
+          Reply
+        </Button>
+      </form>
+    </Form>
   );
 };
 
