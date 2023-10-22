b584dc30f93f32146863bddb98d14a511e425266
Harshit Aditya <harshitaditya27@gmail.com>
2023-10-22 11:31:52 +0530

    Added fetchPost functionality to homepage

--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -34,3 +34,26 @@ export async function createThread({
     throw new Error(`Error creating thread: ${error.message}`);
   }
 }
+
+export async fetchPosts(pageNumber=1,pageSize=20){
+  connectToDB()
+  const skipAmount = (pageNumber - 1)*pageSize; 
+
+  const postsQuery = Thread.find({parentId:{$in:[null,undefined]}}) 
+  .sort({createdAt:"desc"}).skip(skipAmount).limit(pageSize).populate({path:'author', modal:User}) 
+  .populate({ 
+  path:'children', 
+  populate:{  
+    path:'author',
+    model:User, 
+    select:"_id name parentId image"
+  } 
+})  
+const totalPostsCount = await Thread.countDocuments({parentId:{$in:[null,undefined]}}) 
+
+const posts = await postsQuery.exec() 
+
+const isNext = totalPostsCount > skipAmount + posts.length; 
+
+return {posts,isNext}
+}
