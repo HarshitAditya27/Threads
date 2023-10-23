314c2bb7cec4197a046e5102f6e0c90250fa5c8e
Harshit Aditya <harshitaditya27@gmail.com>
2023-10-23 21:29:03 +0530

    Added activity component and fixed authorization box position

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -51,3 +51,37 @@ export async function fetchUser(userId: string) {
     throw new Error(`Failed to fetch user: ${error.message}`);
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+    const userThreads = await Thread.find({ author: uderId });
+
+    // const userThreadss = [
+    //   {
+    //     id: 1,
+    //     children: ["This is great", "This is bad", "This is terrible"],
+    //   },
+    //   {
+    //     id: 2,
+    //     children: ["Ok", "bad"],
+    //   },
+    // ];
+
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+    return replies;
+  } catch (error: any) {
+    throw new Error(`Failed t fetch activity:${error.message}`);
+  }
+}
