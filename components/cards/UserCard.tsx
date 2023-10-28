4403cc30b332cf9bb33f1bcc43fad49f713182a0
Harshit Aditya <71604531+HarshitAditya27@users.noreply.github.com>
2023-10-28 22:09:27 +0530

    Fixed community user routing error

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -37,7 +37,7 @@ const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
           if (isCommunity) {
             router.push(`/communities/${id}`);
           } else {
-            router.push(`profile/${id}`);
+            router.push(`/profile/${id}`);
           }
         }}
       >
